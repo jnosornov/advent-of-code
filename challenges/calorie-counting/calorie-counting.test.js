b00d1fd5d18508e2293290fa4e600ad5db51c9df
Johnathan Osorno <jnosornov@gmail.com>
2023-06-15 01:12:56 -0500

    avoid stdout in logFruits not ignored by suppress logs

--- challenges/calorie-counting/calorie-counting.test.js
+++ challenges/calorie-counting/calorie-counting.test.js
@@ -5,6 +5,7 @@ describe("Calorie Counting", () => {
   it("matches the most calories carried by an Elf", async () => {
     const test = 24000
     const { fruit1 } = await CalorieCounting({ fruit: "1" })
+    console.log("test 1", fruit1)
 
     equal(fruit1.elfCalories, test)
   })
