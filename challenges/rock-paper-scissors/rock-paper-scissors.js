5d1c4c31a409f1ff8ecf2c51e31aca1b1e677a9a
Johnathan Osorno <jnosornov@gmail.com>
2023-06-15 21:17:40 -0500

    challenge 2, 2022, part 2

--- challenges/rock-paper-scissors/rock-paper-scissors.js
+++ challenges/rock-paper-scissors/rock-paper-scissors.js
@@ -1,17 +1,20 @@
 import chalk from "chalk"
+import numeral from "numeral"
 import { run, logFruits, collectFruits } from "../../helpers/general.js"
 import { NEW_LINE, EMPTY_SPACE } from "../../constants.js"
 import { getFileContent } from "../../helpers/file.js"
 
 class RockPaperScissors {
-  constructor({ rounds, scoring }) {
+  constructor({ rounds, scoring, useStrategy = false }) {
     this.score = 0
     this.rounds = rounds
     this.scoring = scoring
+    this.useStrategy = useStrategy
     this.gameShapes = { rock: "R", paper: "P", scissors: "S" }
+    this.roundOutcome = { lose: "X", draw: "Y", win: "Z" }
   }
   
-  #mapToGameShapes(shape) {
+  #mapToGameShape(value) {
     const obj = {
       A: "rock",
       B: "paper",
@@ -21,7 +24,83 @@ class RockPaperScissors {
       Z: "scissors"
     }
 
-    return this.gameShapes[obj[shape]]
+    return this.gameShapes[obj[value]]
+  }
+
+  #mapStrategyToGameShape(round) {
+    const { elf: elfShape, me: strategy } = round
+
+    if (strategy === this.roundOutcome.lose) {
+      if ((elfShape === this.gameShapes.rock)) {
+        return this.gameShapes.scissors
+      }
+
+      if ((elfShape === this.gameShapes.paper)) {
+        return this.gameShapes.rock
+      }
+
+      if ((elfShape === this.gameShapes.scissors)) {
+        return this.gameShapes.paper
+      }
+    }
+
+    if (strategy === this.roundOutcome.draw) {
+      return elfShape
+    }
+
+    if (strategy === this.roundOutcome.win) {
+      if ((elfShape === this.gameShapes.rock)) {
+        return this.gameShapes.paper
+      }
+
+      if ((elfShape === this.gameShapes.paper)) {
+        return this.gameShapes.scissors
+      }
+
+      if ((elfShape === this.gameShapes.scissors)) {
+        return this.gameShapes.rock
+      }
+    }
+  }
+
+  #mapRoundToGameShapes(round) {
+    if (this.useStrategy) {
+      return Object
+        .values(round)
+        .reduce((accum, value, index, array) => {
+          const elfShape = this.#mapToGameShape(array[0])
+
+          if (index === 0) {
+            return {
+              ...accum,
+              elf: elfShape
+            }
+          }
+          
+          return {
+            ...accum,
+            me: this.#mapStrategyToGameShape({ elf: elfShape, me: value })
+          }
+      }, {})
+    }
+
+    return Object
+      .values(round)
+      .reduce((accum, value, index) => {
+        const gameShape = this.#mapToGameShape(value)
+
+        if (index === 0) {
+          return {
+            ...accum,
+            elf: gameShape
+          }
+        }
+        
+        return {
+          ...accum,
+          me: gameShape
+        }
+    }, {})
   }
 
   #scoringRules(shapeValue, elfShapeValue) {
@@ -71,24 +150,7 @@ class RockPaperScissors {
 
   play() {
     for (let i = 0; i < this.rounds.length; i++) {
-      const round = Object
-        .values(this.rounds[i])
-        .reduce((accum, shape, index) => {
-          const gameShape = this.#mapToGameShapes(shape)
-
-          if (index === 0) {
-            return {
-              ...accum,
-              elf: gameShape
-            }
-          }
-          
-          return {
-            ...accum,
-            me: gameShape
-          }
-      }, {})
-
+      const round = this.#mapRoundToGameShapes(this.rounds[i])
       this.#computeRoundOutcome({ round, index: i })
     }
 
@@ -138,19 +200,45 @@ export default async function init({ fruit }) {
       gameScore: Tournament.play()
     }
   }
+
+  function fruitTwo() {
+    const Tournament = new RockPaperScissors({
+      rounds,
+      scoring: {
+        shape: {
+          rock: 1,
+          paper: 2,
+          scissors: 3
+        },
+        roundOutcome: {
+          lost: 0,
+          draw: 3,
+          won: 6
+        }
+      },
+      useStrategy: true
+    })
+
+    return {
+      gameScore: Tournament.play()
+    }
+  }
   
 
-  const fruits = collectFruits({ fruit, callbacks: [fruitOne]})
-  const { fruit1 } = fruits
+  const fruits = collectFruits({ fruit, callbacks: [fruitOne, fruitTwo]})
+  const { fruit1, fruit2 } = fruits
 
   logFruits({
     title: "Rock Paper Scissors",
     fruitOne: {
-      message: `after playing all the rounds the score is ${chalk.yellow(fruit1.gameScore)} points`
+      message: fruit1 ? `after playing all the rounds the score is ${chalk.yellow(numeral(fruit1.gameScore).format("0,0"))} points` : null
+    },
+    fruitTwo: {
+      message: fruit2 ? `after playing all the rounds following the strategy guide the score is ${chalk.yellow(numeral(fruit2.gameScore).format("0,0"))} points` : null
     }
   })
 
   return fruits
 }
 
-run(() => init({ fruit: "1" }))
+run(() => init({ fruit: "both" }))
