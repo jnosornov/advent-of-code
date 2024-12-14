2b706cda1a89ce59d78929cb798560d21aacc400
Johnathan Osorno <jnosornov@gmail.com>
2024-12-13 19:37:12 -0500

    solve the first challenge

---
+++ src/challenges/mull-it-over/mull-it-over.js
@@ -0,0 +1,73 @@
+import chalk from "chalk"
+import numeral from "numeral"
+import { collectFruits, logFruits, run } from "../../helpers/general.js"
+import { getFileContent } from "../../helpers/file.js"
+import { isNumeric } from "../../helpers/tools.js"
+
+export default async function init({ fruit }) {
+  const filename = process.env.NODE_ENV !== "test"
+    ? "./input.sample.txt"
+    : "./input.txt"
+
+  const { contents: corruptedMemory } = await getFileContent({
+    path: new URL(filename, import.meta.url)
+  })
+
+  const fruits = collectFruits({ fruit, callbacks: [() => fruitOne(corruptedMemory)] })
+  const { fruit1 } = fruits
+
+  logFruits({
+    title: "Mull It Over",
+    fruitOne: {
+      message: fruit1 ? `the result of adding the uncorrupted instructions are ${chalk.yellow(numeral(fruit1).format("0,0"))}` : null
+    }
+  })
+
+  return fruits
+}
+
+function fruitOne(memory) {
+  return computeMemory(memory)
+}
+
+export function computeMemory(memory) {
+  let result = 0
+
+  for (let i = 0; i <= memory.length - 1; i++) {
+    const matchesStartOfOperation = `${memory[i]}${memory[i + 1]}${memory[i + 2]}${memory[i + 3]}` === "mul("
+    const multiplicand = getOperand({ memory, index: i + 4 })
+
+    if (multiplicand.length === 0) continue
+    const separatorIdx = i + 4 + multiplicand.length
+    const matchesOperandsSeparator = memory[separatorIdx] === ","
+
+    const multiplier = getOperand({ memory, index: separatorIdx + 1 })
+
+    if (multiplier.length === 0) continue
+    const endOfOperationIdx = separatorIdx + multiplier.length + 1
+    const matchesEndOfOperation = memory[endOfOperationIdx] === ")"
+
+    const isValidInstruction = matchesStartOfOperation && matchesOperandsSeparator && matchesEndOfOperation
+
+    if (!isValidInstruction) continue
+    result = result + (parseInt(multiplicand) * parseInt(multiplier))
+  }
+
+  return result
+}
+
+function getOperand({ memory, index }) {
+  let operand = ""
+  const OPERAND_MAX_LENGTH = 3
+
+  for (let j = 0; j <= OPERAND_MAX_LENGTH - 1; j++) {
+    const item = memory[index + j]
+
+    if (!isNumeric(item)) return operand
+    operand = `${operand}${item}`
+  }
+
+  return operand
+}
+
+run(() => init({ fruit: "1" }))
--- src/helpers/tools.test.js
+++ src/helpers/tools.test.js
@@ -1,25 +1,41 @@
-import { deepEqual } from "assert"
-import { removeListItem } from "./tools.js"
+import { equal, deepEqual } from "assert"
+import { removeListItem, isNumeric } from "./tools.js"
 
-describe("removeListItem", () => {
-  it("it should remove the item out of the list", () => {
-    const updatedList = removeListItem({ list: ["4", "10", "15", "3"], index: 2 })
-    const expected = ["4", "10", "3"]
+describe("tools", () => {
+  describe("removeListItem", () => {
+    it("should remove the item out of the list", () => {
+      const updatedList = removeListItem({ list: ["4", "10", "15", "3"], index: 2 })
+      const expected = ["4", "10", "3"]
 
-    deepEqual(updatedList, expected)
+      deepEqual(updatedList, expected)
+    })
+
+    it("should remove the first item in the list", () => {
+      const updatedList = removeListItem({ list: ["4", "10", "15", "3"], index: 0 })
+      const expected = ["10", "15", "3"]
+
+      deepEqual(updatedList, expected)
+    })
+
+    it("should remove the last item in the list", () => {
+      const updatedList = removeListItem({ list: ["4", "10", "15", "3"], index: 3 })
+      const expected = ["4", "10", "15"]
+
+      deepEqual(updatedList, expected)
+    })
   })
 
-  it("it should remove the first item in the list", () => {
-    const updatedList = removeListItem({ list: ["4", "10", "15", "3"], index: 0 })
-    const expected = ["10", "15", "3"]
+  describe("isNumeric", () => {
+    it("should return false when string is not numeric", () => {
+      const text = "r"
 
-    deepEqual(updatedList, expected)
-  })
+      equal(isNumeric(text), false)
+    })
 
-  it("it should remove the last item in the list", () => {
-    const updatedList = removeListItem({ list: ["4", "10", "15", "3"], index: 3 })
-    const expected = ["4", "10", "15"]
+    it("should return true when string is numeric", () => {
+      const text = "9"
 
-    deepEqual(updatedList, expected)
+      equal(isNumeric(text), true)
+    })
   })
 })
