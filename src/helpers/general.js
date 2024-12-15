a352e5a075402dd226ea34289ff54325e2ee3abf
Johnathan Osorno <jnosornov@gmail.com>
2024-12-15 14:31:35 -0500

    move logic away from mull it over challenge

--- src/helpers/general.js
+++ src/helpers/general.js
@@ -1,52 +1,60 @@
 import chalk from "chalk"
+import numeral from "numeral"
+import { getFileContent } from "./file.js"
 import { FRUIT_POINTER } from "../constants.js"
+
+async function runChallenge({ star, challenge, solutions, directory, opts }) {
+  const puzzle = process.env.NODE_ENV === "test"
+    ? "./input.sample.txt"
+    : "./input.txt"
+
+  const { contents } = await getFileContent({ path: `${directory}/${puzzle}`, opts })
+  const stars = collectStars({
+    star,
+    contents,
+    callbacks: solutions
+  })
+
+  logStars({ challenge, stars })
+  return stars
+}
 
 function run(callback) {
   const { NODE_ENV } = process.env
-
   if (NODE_ENV === "test") return
   callback()
 }
 
-function collectFruits({ fruit = "1", callbacks = [] }) {
-  if (!callbacks.length) return
+function collectStars({ star, contents, callbacks = [] }) {
+  if (!callbacks.length) return []
   const [cb1, cb2] = callbacks
 
-  if (fruit === "1" && cb1) {
-    return {
-      fruit1: cb1()
-    }
+  if (star === "1" && cb1) return [cb1(contents)]
+  if (star === "2" && cb2) return [cb2(contents)]
+
+  if (star === "both" && (cb1 && cb2)) {
+    return [cb1(contents), cb2(contents)]
   }
 
-  if (fruit === "2" && cb2) {
-    return {
-      fruit2: cb2()
-    }
-  }
-
-  if (fruit === "both" && (cb1 && cb2)) {
-    return {
-      fruit1: cb1(),
-      fruit2: cb2()
-    }
-  }
+  return []
 }
 
-function logFruits({ title, fruitOne, fruitTwo }) {
-  console.log(chalk.bold(title))
+function logStars({ challenge = "", stars = [] }) {
+  const [first, second] = stars
+
+  console.log("\r")
+  console.log(chalk.bold(challenge))
   console.log("\r")
 
-  if (fruitOne?.message) {
-    console.log(`${FRUIT_POINTER} ${fruitOne?.message}`)
+  if (first !== undefined) {
+    console.log(`${FRUIT_POINTER} 1: ${chalk.yellow(numeral(first).format("0,0"))}`)
   }
 
-  if (fruitTwo?.message) {
-    console.log(`${FRUIT_POINTER} ${fruitTwo?.message}`)
+  if (second !== undefined) {
+    console.log(`${FRUIT_POINTER} 2: ${chalk.yellow(numeral(second).format("0,0"))}`)
   }
+
+  console.log("\r")
 }
 
-export {
-  run,
-  collectFruits,
-  logFruits
-}
+export { run, runChallenge }
